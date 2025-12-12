4aac2875344c64ff4c52b77ee71a8e3452a02ef8
marchauptmann1984 <65250928+marchauptmann1984@users.noreply.github.com>
2025-12-13 00:11:23 +0100

    Update test_history.py
    Longer term history for robustness

--- AppDaemon_deploy/test_history.py
+++ AppDaemon_deploy/test_history.py
@@ -11,7 +11,9 @@ class HistoryExample(hass.Hass):
 
     def initialize(self):
         # set API parameters
+        dayz=14
         parameters = {
+            "past_days": dayz,
             "latitude": 51.3225,
             "longitude": 4.9447,
             "tilt": 60,
@@ -19,12 +21,13 @@ class HistoryExample(hass.Hass):
             "hourly": ['relative_humidity_2m', 'precipitation', 'cloud_cover_low', 'cloud_cover_mid', 'cloud_cover_high' , 'soil_temperature_0cm', 'snow_depth', 'global_tilted_irradiance'],
         }
         parameters_2 = {
+                "past_days": dayz,
                 "latitude": 51.3225,
-                    "longitude": 4.9447,
-                    "tilt": 60,
-                    "azimuth": -103,
-                    "hourly": ['global_tilted_irradiance'],
-                }    
+                "longitude": 4.9447,
+                "tilt": 60,
+                "azimuth": -103,
+                "hourly": ['global_tilted_irradiance'],
+        }    
         # time zone
         tz_name = self.args.get("tz", "Europe/Amsterdam") 
         # change to your HA timezone 
@@ -33,14 +36,13 @@ class HistoryExample(hass.Hass):
         # Build today's 23:59 in local time
         run_time = tz.localize(datetime.combine(now.date()-timedelta(days=1), time(22, 30)))        
         print(run_time)
-        self.run_daily(self.calc_mse, run_time, tzinfo=tz, tzname=tz_name, pars=parameters, pars2=parameters_2, url="yy", key="xx")
-    def update_model(self, df_new, url, key, para, para_2):
-        #print(df_new)
-        #print(url)
-        #print(key)
-        #print(para)
-        #print(para_2)
-        # get weather forecast
+        self.run_daily(self.calc_mse, run_time, tzinfo=tz, daysback=dayz, tzname=tz_name, pars=parameters, pars2=parameters_2, url_i="https://ehklxvz47g.execute-api.eu-north-1.amazonaws.com/prod/predict", url_t="https://ehklxvz47g.execute-api.eu-north-1.amazonaws.com/prod/retrain", key="my-home-automation-secret")
+        #self.run_every(self.calc_mse, now, 120, daysback=dayz, tzname=tz_name, pars=parameters, pars2=parameters_2, url_i="https://ehklxvz47g.execute-api.eu-north-1.amazonaws.com/prod/predict", url_t="https://ehklxvz47g.execute-api.eu-north-1.amazonaws.com/prod/retrain", key="my-home-automation-secret")
+    
+    def days_in_year(self, year=datetime.now().year):
+            return 365 + calendar.isleap(year)
+    
+    def get_forecast(self, para, para_2):
         response = requests.get("https://api.open-meteo.com/v1/forecast",params=para)
         weather_fc_int=pd.DataFrame(response.json()['hourly'])
         response_roof2 = requests.get("https://api.open-meteo.com/v1/forecast",params=para_2)
@@ -49,15 +51,12 @@ class HistoryExample(hass.Hass):
         weather_fc_int['is_day']=weather_fc_int.global_tilted_irradiance>0
         weather_fc_int['time_']=weather_fc_int['time']
         weather_fc_int['time'] = pd.to_datetime(weather_fc_int['time'])
-        weather_fc_int['checktime']=weather_fc_int['time']
-        def days_in_year(year=datetime.now().year):
-            return 365 + calendar.isleap(year)
-        days_in_year(year=2024)
-        weather_fc_int['time']=(weather_fc_int.time.dt.year-2024)*days_in_year(year=2024)*24+weather_fc_int.time.dt.day_of_year*24+weather_fc_int.time.dt.hour
+        #weather_fc_int['checktime']=weather_fc_int['time']
+        weather_fc_int['time']=(weather_fc_int.time.dt.year-2024)*self.days_in_year(year=2024)*24+weather_fc_int.time.dt.day_of_year*24+weather_fc_int.time.dt.hour
         weather_fc_int.set_index('time', inplace=True)
         weather_fc_int.columns=['relative_humidity_2m', 'precipitation', 'cloud_cover_low',
             'cloud_cover_mid', 'cloud_cover_high', 'soil_temperature_0_to_7cm', 'snow_depth', 'global_tilted_irradiance',
-            'is_day', 'time_', 'checktime']
+            'is_day', 'time_']
         # make 2 versions at begin and end of interval
         weather_fc_int_1=weather_fc_int.loc[weather_fc_int.index[:-1]]
         weather_fc_int_2=weather_fc_int.loc[weather_fc_int.index[:-1]+1]
@@ -73,52 +72,28 @@ class HistoryExample(hass.Hass):
         weather_fc_int.snow_depth=(weather_fc_int_1.snow_depth+weather_fc_int_2.snow_depth)/2
         weather_fc_int.loc[weather_fc_int.snow_depth<0, 'snow_depth']=0
         weather_fc_int.loc[np.isnan(weather_fc_int.snow_depth), 'snow_depth']=0
+        Time=pd.to_datetime(weather_fc_int_1.time_)
         weather_fc_int.drop('time_', axis=1,inplace=True)
         data_fc_comb=weather_fc_int
-        # match targets and input parameters
-        df_new.timestamp=pd.to_datetime(df_new["timestamp"])
-        df_new['time']=(df_new.timestamp.dt.year-2024)*days_in_year(year=2024)*24+df_new.timestamp.dt.day_of_year*24+df_new.timestamp.dt.hour
-        df_new.set_index('time', inplace=True)
-        print(data_fc_comb)
-        print(df_new)
-        # split data        
-        is_day = data_fc_comb.loc[df_new.index].is_day
-        X_train = data_fc_comb.loc[df_new.index].iloc[:, [0, 1, 2, 3, 4, 5, 6, 7]]        
-        X_train = X_train[is_day]      
-        y_train = df_new.loc[X_train.index].actual_kwh.tolist()
-        X_train = X_train.values.tolist() 
-        print(len(X_train))
-        print(len(y_train))
-        # API call
-        headers = {
-            "Content-Type": "application/json",
-            "x-api-key": key
-        }       
-        payload = {
-            "features": X_train,
-            "targets": y_train
-        }
-        response = requests.post(url, json=payload, headers=headers)
-        try:
-            print(response.json())
-        except Exception:
-            print(response.text)
-
+        data_fc_comb = data_fc_comb.astype(float)
+        return data_fc_comb, Time
+    
     def calc_mse(self, kwargs):
+        days_back=kwargs["daysback"]
         #print("start")
         # timezone-aware "now" from AppDaemon
         tz_name = kwargs["tzname"] 
         #print(tz_name)
         power = kwargs.get("entity_id", "sensor.daily_power_generated")
         forecast = kwargs.get("entity_id", "sensor.solar_forecast_hourly")
-        now = self.get_now()
-        #print(now)
+        now = self.get_now().astimezone(pytz.UTC)
+        print(now)
         # start of local day (00:00:00)
-        start_of_day = now.replace(hour=0, minute=0, second=0, microsecond=0)
+        start_of_window = (now-timedelta(days=days_back)).replace(hour=0, minute=0, second=0, microsecond=0)
 
         # fetch history starting one day before midnight to ensure we have a value
         # *before* the first boundary (this fixes the "first entry" problem)
-        history_start = start_of_day - timedelta(days=1)
+        history_start = start_of_window - timedelta(hours=1)
 
         history = self.get_history(entity_id=power, start_time=history_start, end_time=now)
         
@@ -133,10 +108,11 @@ class HistoryExample(hass.Hass):
 
         # turn into DataFrame for easier timestamp handling
         df = pd.DataFrame(entries)
-        df["last_changed"] = pd.to_datetime(df["last_changed"])
+        #print(df.last_changed.head(50))
+        df["last_changed"] = pd.to_datetime(df["last_changed"], utc=True) # to utc
         df["state"] = pd.to_numeric(df["state"], errors="coerce")
         df = df.dropna(subset=["state"]).sort_values("last_changed").reset_index(drop=True)
-        #print(df.last_changed.head(10))
+        print(df.last_changed.head(50))
 
         if df.empty:
             self.log("No numeric states in history.")
@@ -153,13 +129,14 @@ class HistoryExample(hass.Hass):
 
         # build hourly buckets from 00:00 up to now (include current partial hour)
         hour_starts = []
-        t = start_of_day
+        t = start_of_window
         while t <= now:
             hour_starts.append(t)
             t = t + timedelta(hours=1)
-
+        #print(hour_starts)
         hours = []
         energies = []
+        days = []
         for h_start in hour_starts:
             h_end = h_start + timedelta(hours=1)
             if h_end > now:
@@ -191,59 +168,60 @@ class HistoryExample(hass.Hass):
                     energy = float(v_end)
 
             hours.append(h_start.hour)
+            days.append(h_start.day)
             energies.append(energy)
         print (energies)
         print (hours)
-
-        # get attributes from forecast sensor
-        state_data = self.get_state(forecast, attribute="all")
-        if not state_data or "attributes" not in state_data:
-            self.log("No forecast data found.")
-            return
-        attrs = state_data["attributes"]
-        peak_times = attrs.get("PeakTimes", [])
-        peak_heights = attrs.get("PeakHeights", [])
-        print(peak_times[6])
-        print(peak_heights[6])
-        if not peak_times or not peak_heights or len(peak_times) != len(peak_heights):
-            self.log("Forecast arrays missing or lengths do not match.")
-            return
-        df_forecast = pd.DataFrame({
-            "datetime": pd.to_datetime(peak_times).tz_convert(pytz.timezone(tz_name)),
-            "datetime_org": pd.to_datetime(peak_times),
-            "forecast": peak_heights
-        })
-        print(df_forecast.head(7))
-        # filter for today in local time
-        today_local = datetime.now(pytz.timezone(tz_name)).date()
-        df_forecast = df_forecast[df_forecast["datetime"].dt.date == today_local]
-        forecast_timestamp = df_forecast["datetime_org"]
-        forecast_hours = df_forecast["datetime"].dt.hour.tolist()
-        forecast_values = df_forecast["forecast"].tolist()
-        print(forecast_values)
-        print(forecast_hours)
-
+        print (days)
+        # get forecast
+        data_fc_comb, Time=self.get_forecast(kwargs["pars"], kwargs["pars2"])
+        print(data_fc_comb)
+         # prediction
+        X_in = data_fc_comb[data_fc_comb.is_day>0].iloc[:, 0:8].values.tolist()
+        url = kwargs["url_i"]
+        headers = {
+            "Content-Type": "application/json",
+            "x-api-key": kwargs["key"]
+        }       
+        payload = {
+            "features": X_in
+        }
+        response = requests.post(url, json=payload, headers=headers)
+        try:    
+            #print(response.json())
+            resp=response.json()
+            predictions=resp['prediction']
+        except Exception:
+            print(response.text)
+        df_fc=pd.DataFrame(0, columns=['timestamp','forecast_kwh'], index=data_fc_comb.index)
+        df_fc['forecast_kwh'] = df_fc['forecast_kwh'].astype('float')
+        df_fc.loc[data_fc_comb.is_day>0, "forecast_kwh"]=predictions  
+        df_fc.timestamp=pd.to_datetime(Time)
+        print(df_fc.head(20)) 
+        # put back actual prediction input data
+        df_fc = df_fc.join(data_fc_comb)   
+        df_fc['day'] = df_fc.timestamp.dt.day.tolist()  
+        df_fc['hour'] = df_fc.timestamp.dt.hour.tolist()  
+        print("bro")
+        print(df_fc.head(20))  
         # merge
         # Build actual energy DataFrame
         df_actual = pd.DataFrame({
+            "day": days,
             "hour": hours,
             "actual_kwh": energies
         })
+        print("motherfucker")
         print(df_actual)
-        # Build forecast DataFrame (hour in local timezone)
-        df_forecast_local = pd.DataFrame({
-            "hour": forecast_hours,
-            "forecast_kwh": forecast_values,
-            "timestamp": forecast_timestamp
-        })
         # Merge on hour using inner join to keep only overlapping hours
-        df_combined = pd.merge(df_actual, df_forecast_local, on="hour", how="inner")
+        df_combined = pd.merge(df_actual, df_fc, on=["hour", "day"], how="inner")
 
         # Optional: sort by hour
-        df_combined = df_combined.sort_values("hour").reset_index(drop=True)
+        df_combined = df_combined.sort_values(["day","hour"]).reset_index(drop=True)
+        print(df_combined)
         #df_combined.to_csv("/config/my_data.csv", index=False)
-        df_combined=df_combined[df_combined.forecast_kwh>0.0001]
-        print(df_combined)        
+        df_combined=df_combined[df_combined.forecast_kwh>0.0]
+        print(df_combined.iloc[:, 0:5].tail(24))        
         # ------ calculate mse between prediction and actual
         # Extract series
         y_true = df_combined['actual_kwh']
@@ -256,6 +234,26 @@ class HistoryExample(hass.Hass):
 
         if rmse>0.3:
            print("RMSE is too high! Initiate retraining.")
-           self.update_model(df_combined, kwargs["url"], kwargs["key"], kwargs["pars"], kwargs["pars2"])
+           X_train = df_combined.iloc[:, [5, 6, 7, 8, 9, 10, 11, 12]]
+           print(X_train.columns)
+           print(X_train)        
+           y_train = df_combined.actual_kwh.tolist()
+           X_train = X_train.values.tolist() 
+           print(len(X_train))
+           print(len(y_train))
+           # API call
+           headers = {
+               "Content-Type": "application/json",
+               "x-api-key": kwargs["key"]
+           }       
+           payload = {
+               "features": X_train,
+               "targets": y_train
+           }
+           response = requests.post(kwargs["url_t"], json=payload, headers=headers)
+           try:
+               print(response.json())
+           except Exception:
+               print(response.text)                       
         else:
            print("RMSE is acceptable. Retraining not required.")
